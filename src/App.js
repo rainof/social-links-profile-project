9bf8d6619f09b2c82772722cf19de285b10ecbba
rainof <13416303+rainof@users.noreply.github.com>
2024-07-27 23:53:46 +0700

    Set up project structure and add initial information

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import pic from './assets/images/avatar-jessica.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Jessica Randall
+        London, United Kingdom
+        "Front-end developer and avid reader."
+        GitHub
+        Frontend Mentor
+        LinkedIn
+        Twitter
+        Instagram */}
+
+        <div className="card" style={{width: '18rem'}}>
+          <div className="profile">
+            <img src={pic} alt="Jessica Randall" />
+            <h5 className="profile-name">Jessica Randall</h5>
+            <h5 className="profile-location">London, United Kingdom</h5>
+            <h5 className="profile-bio">"Front-end developer and avid reader."</h5>
+          </div>
+          <div className="button-link">
+            <button className="btn btn-primary" href="#">GitHub</button>
+            <button className="btn btn-primary" href="#">Frontend Mentor</button>
+            <button className="btn btn-primary" href="#">LinkedIn</button>
+            <button className="btn btn-primary" href="#">Twitter</button>
+            <button className="btn btn-primary" href="#">Instagram</button>
+          </div>
+        </div>
       </header>
     </div>
   );
