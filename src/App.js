c12af33c550afb26021dc4b0d3d8247dc78905ae
rainof <13416303+rainof@users.noreply.github.com>
2024-07-28 12:41:45 +0700

    Update button styling with hovering and improve positioning

--- src/App.js
+++ src/App.js
@@ -15,19 +15,19 @@ function App() {
         Twitter
         Instagram */}
 
-        <div className="card" style={{width: '18rem'}}>
+        <div className="card" style={{width: '20rem'}}>
           <div className="profile">
-            <img src={pic} alt="Jessica Randall" />
+            <img className="profile-image" src={pic} alt="Jessica Randall" />
             <h5 className="profile-name">Jessica Randall</h5>
             <h5 className="profile-location">London, United Kingdom</h5>
             <h5 className="profile-bio">"Front-end developer and avid reader."</h5>
           </div>
           <div className="button-link">
-            <button className="btn btn-primary" href="#">GitHub</button>
-            <button className="btn btn-primary" href="#">Frontend Mentor</button>
-            <button className="btn btn-primary" href="#">LinkedIn</button>
-            <button className="btn btn-primary" href="#">Twitter</button>
-            <button className="btn btn-primary" href="#">Instagram</button>
+            <button className="button-ind" href="#">GitHub</button>
+            <button className="button-ind" href="#">Frontend Mentor</button>
+            <button className="button-ind" href="#">LinkedIn</button>
+            <button className="button-ind" href="#">Twitter</button>
+            <button className="button-ind" href="#">Instagram</button>
           </div>
         </div>
       </header>
